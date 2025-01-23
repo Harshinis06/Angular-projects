6e080a635720b878c417110062cb125511323dd0
Harshinis06 <harshini20010206@gmail.com>
2025-01-23 11:15:11 +0530

    Add files via upload

---
+++ src/app/addrecipe/addrecipe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addrecipe',
+  templateUrl: './addrecipe.component.html',
+  styleUrls: ['./addrecipe.component.css']
+})
+export class AddrecipeComponent {
+
+}
